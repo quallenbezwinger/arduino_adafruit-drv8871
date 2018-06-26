38f797c5a31046fb1a2ab0cbe555d2610a6f188e
dirk.koehler <dirk.koehler@cardioscan.de>
2018-06-26 07:42:51 +0200

    formating constants

--- DRV8871.h
+++ DRV8871.h
@@ -14,9 +14,9 @@ class DRV8871
 {
   public:
     DRV8871 (byte motorIN1Pin, byte motorIN2Pin);
-    const byte DIRECTION_BACKWARD = 1;
-    const byte DIRECTION_FORWARD = 2;
     const byte DIRECTION_NONE = 0;
+    const byte DIRECTION_FORWARD = 1;
+    const byte DIRECTION_BACKWARD = 2;
     void drive(byte targetSpeed, byte direction, int acceleration);
     void breakdown(byte targetSpeed = 0, int acceleration = 0);
     byte currentSpeed();
